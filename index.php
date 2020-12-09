e3537e55f6ead90fbc566888ce5ee0376b5c50e0
jerp86 <jerp4@hotmail.com>
2020-12-09 20:10:54 -0300

    Usando comando de Repetição

--- index.php
+++ index.php
@@ -8,9 +8,18 @@ $nome = 'José';
 $idade = 34;
 
 if ($idade >= 6 && $idade <= 12) {
-	echo 'infantil';
+	for ($i = 0; $i <= count($categorias); $i++) {
+		if ($categorias[$i] == 'infantil')
+			echo "O nadador ".$nome." compete na categoria infantil";
+	}
 } else if ($idade >= 13 && $idade <= 18) {
-	echo 'adolescente';
+	for ($i = 0; $i <= count($categorias); $i++) {
+		if ($categorias[$i] == 'adolescente')
+			echo "O nadador ".$nome." compete na categoria adolescente";
+	}
 } else {
-	echo 'adulto';
+	for ($i = 0; $i <= count($categorias); $i++) {
+		if ($categorias[$i] == 'adulto')
+			echo "O nadador ".$nome." compete na categoria adulto";
+	}
 }
