5bdde03420b27dca323691583b236ea01ebf63a9
jerp86 <jerp4@hotmail.com>
2020-12-09 20:09:37 -0300

    Usando Condicionais, Comparadores e Lógicos

--- index.php
+++ index.php
@@ -4,10 +4,13 @@ $categorias[] = 'infantil';
 $categorias[] = 'adolescente';
 $categorias[] = 'adulto';
 
-// print_r($categorias); // mostra informações sobre a variável
-
 $nome = 'José';
 $idade = 34;
 
-var_dump($nome); // mostra o Tipo e tamanho da variável
-echo $idade; // mostra o valor da variável
+if ($idade >= 6 && $idade <= 12) {
+	echo 'infantil';
+} else if ($idade >= 13 && $idade <= 18) {
+	echo 'adolescente';
+} else {
+	echo 'adulto';
+}
