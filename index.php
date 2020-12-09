fd7b2153e8d61e1a156cbb03f1bcea0e3630ed57
jerp86 <jerp4@hotmail.com>
2020-12-09 20:07:38 -0300

    Aprendendo outros comandos de saída

--- index.php
+++ index.php
@@ -4,4 +4,10 @@ $categorias[] = 'infantil';
 $categorias[] = 'adolescente';
 $categorias[] = 'adulto';
 
-print_r($categorias);
+// print_r($categorias); // mostra informações sobre a variável
+
+$nome = 'José';
+$idade = 34;
+
+var_dump($nome); // mostra o Tipo e tamanho da variável
+echo $idade; // mostra o valor da variável
