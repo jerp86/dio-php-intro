56e83cb08aed583995d7b270669715b1d8050481
jerp86 <jerp4@hotmail.com>
2020-12-10 20:54:15 -0300

    Acressentando return

--- script.php
+++ script.php
@@ -40,21 +40,24 @@ if ($idade >= 6 && $idade <= 12) {
 	for ($i = 0; $i <= count($categorias); $i++) {
 		if ($categorias[$i] == 'infantil') {
 			$_SESSION['mensagem-de-sucesso'] = "O nadador ".$nome." compete na categoria ".$categorias[$i];
-			header("location: index.php")
+			header("location: index.php");
+			return;
 		}
 	}
 } else if ($idade >= 13 && $idade <= 18) {
 	for ($i = 0; $i <= count($categorias); $i++) {
 		if ($categorias[$i] == 'adolescente') {
 			$_SESSION['mensagem-de-sucesso'] = "O nadador ".$nome." compete na categoria ".$categorias[$i];
-			header("location: index.php")
+			header("location: index.php");
+			return;
 		}
 	}
 } else {
 	for ($i = 0; $i <= count($categorias); $i++) {
 		if ($categorias[$i] == 'adulto') {
 			$_SESSION['mensagem-de-sucesso'] = "O nadador ".$nome." compete na categoria ".$categorias[$i];
-			header("location: index.php")
+			header("location: index.php");
+			return;
 		}
 	}
 }
