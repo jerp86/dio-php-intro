ef48db1ba81d6384b46a62516a8b2375bc99ee84
jerp86 <jerp4@hotmail.com>
2020-12-10 12:07:36 -0300

    Começando a validar os inputs do usuário

--- script.php
+++ script.php
@@ -7,6 +7,21 @@ $categorias[] = 'adulto';
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
+if (empty($nome)) {
+	echo "O nome não pode ser vazio";
+	return;
+}
+
+if (strlen($nome < 3)) {
+	echo "O nome deve conter mais de 3 caracteres";
+	return;
+}
+
+if (strlen($nome) > 40) {
+	echo "O nome é muito extenso";
+	return;
+}
+
 if ($idade >= 6 && $idade <= 12) {
 	for ($i = 0; $i <= count($categorias); $i++) {
 		if ($categorias[$i] == 'infantil')
