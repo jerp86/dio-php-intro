95dcb376b7fe77a7e88c0ccafd6a73e4fd498194
jerp86 <jerp4@hotmail.com>
2020-12-11 06:30:49 -0300

    Combinando condições com funções e retornos

--- script.php
+++ script.php
@@ -2,36 +2,5 @@
 
 session_start();
 
-$categorias = [];
-$categorias[] = 'infantil';
-$categorias[] = 'adolescente';
-$categorias[] = 'adulto';
-
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
-
-if ($idade >= 6 && $idade <= 12) {
-	for ($i = 0; $i <= count($categorias); $i++) {
-		if ($categorias[$i] == 'infantil') {
-			$_SESSION['mensagem-de-sucesso'] = "O nadador ".$nome." compete na categoria ".$categorias[$i];
-			header("location: index.php");
-			return;
-		}
-	}
-} else if ($idade >= 13 && $idade <= 18) {
-	for ($i = 0; $i <= count($categorias); $i++) {
-		if ($categorias[$i] == 'adolescente') {
-			$_SESSION['mensagem-de-sucesso'] = "O nadador ".$nome." compete na categoria ".$categorias[$i];
-			header("location: index.php");
-			return;
-		}
-	}
-} else {
-	for ($i = 0; $i <= count($categorias); $i++) {
-		if ($categorias[$i] == 'adulto') {
-			$_SESSION['mensagem-de-sucesso'] = "O nadador ".$nome." compete na categoria ".$categorias[$i];
-			header("location: index.php");
-			return;
-		}
-	}
-}
---
+++ servicos/servicoCategoriaCompetidor.php
@@ -0,0 +1,35 @@
+<?php
+
+function defineCategoriaCompetidor(string $nome, $string $idade) : ?string {
+  $categorias = [];
+  $categorias[] = 'infantil';
+  $categorias[] = 'adolescente';
+  $categorias[] = 'adulto';
+
+  if (validaNome($nome) &&validaIdade($idade)) {
+    if ($idade >= 6 && $idade <= 12) {
+      for ($i = 0; $i <= count($categorias); $i++) {
+        if ($categorias[$i] == 'infantil') {
+          setarMensagemSucesso("O nadador ".$nome." compete na categoria ".$categorias[$i]);
+          return null;
+        }
+      }
+    } else if ($idade >= 13 && $idade <= 18) {
+      for ($i = 0; $i <= count($categorias); $i++) {
+        if ($categorias[$i] == 'adolescente') {
+          setarMensagemSucesso("O nadador ".$nome." compete na categoria ".$categorias[$i]);
+          return null;
+        }
+      }
+    } else {
+      for ($i = 0; $i <= count($categorias); $i++) {
+        if ($categorias[$i] == 'adulto') {
+          setarMensagemSucesso("O nadador ".$nome." compete na categoria ".$categorias[$i]);
+          return null;
+        }
+      }
+    }
+  } else {
+    return obterMensagemErro();
+  }
+}
