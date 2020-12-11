d03aa96dda19eede1aec4d1f9df3380f10dcb978
jerp86 <jerp4@hotmail.com>
2020-12-10 22:52:24 -0300

    Organizando parâmetros diversos nas funções

--- servicos/servicoMensagemSessao.php
+++ servicos/servicoMensagemSessao.php
@@ -9,4 +9,30 @@ function obterMensagemErro() : ?string {
   if (isset($_SESSION['mensagem-de-erro'])) {
     return $_SESSION['mensagem-de-erro'];
   }
+
+  return null;
+}
+
+function setarMensagemSucesso(string $mensagem) : void {
+  $_SESSION['mensagem-de-sucesso'] = $mensagem;
+}
+
+function obterMensagemSucesso() : ?string {
+  if (isset($_SESSION['mensagem-de-sucesso'])) {
+    return $_SESSION['mensagem-de-sucesso'];
+  }
+
+  return null;
+}
+
+function removerMensagemErro() : void {
+  if (isset($_SESSION['mensagem-de-erro'])) {
+    unset($_SESSION['mensagem-de-erro']);
+  }
+}
+
+function remomverMensagemSucesso() : void {
+  if (isset($_SESSION['mensagem-de-sucesso'])) {
+    unset($_SESSION['mensagem-de-sucesso']);
+  }
 }
--- servicos/servicoValidacao.php
+++ servicos/servicoValidacao.php
@@ -2,17 +2,17 @@
 
 function validaNome(string $nome) : bool {
   if (empty($nome)) {
-    $_SESSION['mensagem-de-erro'] = "O nome não pode ser vazio, preencha-o novamente";
+    setarMensagemErro("O nome não pode ser vazio, preencha-o novamente");
     return false;
   }
 
   if (strlen($nome < 3)) {
-    $_SESSION['mensagem-de-erro'] = "O nome deve conter mais de 3 caracteres, preencha-o novamente";
+    setarMensagemErro("O nome deve conter mais de 3 caracteres, preencha-o novamente");
     return false;
   }
 
   if (strlen($nome) > 40) {
-    $_SESSION['mensagem-de-erro'] = "O nome é muito extenso, preencha-o novamente";
+    setarMensagemErro("O nome é muito extenso, preencha-o novamente");
     return false;
   }
 
@@ -21,7 +21,7 @@ function validaNome(string $nome) : bool {
 
 function validaIdade(string $idade) : bool {
   if (!is_numeric($idade)) {
-    $_SESSION['mensagem-de-erro'] = "Informe um número para a idade, preencha-o novamente";
+    setarMensagemErro("Informe um número para a idade, preencha-o novamente");
     return false;
   }
 
