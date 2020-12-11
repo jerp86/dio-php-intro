c183e002a6f8bda08a93d6788c6bb287a384d00c
jerp86 <jerp4@hotmail.com>
2020-12-11 06:54:07 -0300

    Vinculando diversos arquivos PHP

--- script.php
+++ script.php
@@ -1,6 +1,12 @@
 <?php
 
-session_start();
+include "servicos/servicoMensagemSessao.php";
+include "servicos/servicoValidacao.php"
+include "servicos/servicoCategoriaCompetidor.php";
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
+
+defineCategoriaCompetidor($nome, $idade);
+
+header('location: index.php');
